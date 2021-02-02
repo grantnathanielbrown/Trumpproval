b486c4a87082c1076e5f57d1034aeadb9fcc8c27
Grant Brown <grant@Grants-MacBook-Pro.local>
2021-02-02 14:38:22 -0500

    Fixed chronological order on trump tweets csv, got rid of all tweets before january 2017. Retitled some files

---
+++ src/convert/convertTweets.js
@@ -0,0 +1,6 @@
+let tweets = require("/Users/grant/Desktop/Trumpproval/src/convert/data/TrumpTweets_Jan2017-_END - tweets_01-08-2021.csv");
+console.log(tweets.slice(0,100));
+
+function convert(data) {
+    // '
+}
