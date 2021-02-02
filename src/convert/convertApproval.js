1079e63a35185793a0d42502a856f2fc0ac0eb5f
Grant Brown <grant@Grants-MacBook-Pro.local>
2021-02-02 17:48:35 -0500

    Created convertTweets file based on the contents of convertApproval. Created tweets.JSON after running convertTweets file Slightly re- factored convertApproval

--- src/convert/convertApproval.js
+++ src/convert/convertApproval.js
@@ -9,65 +9,65 @@ function getAverageRating(arr) {
 
 
     
-    const stream = fs.createReadStream("/Users/grant/Desktop/Trumpproval/src/convert/data/approval_topline.csv")
-    function convert(results) {
-        
-        let reversedData = results.data.reverse();
-        reversedData.pop();
-        
-        // console.log("Parsing complete:", reversedData);
-        let transformedData = [];
-
-        reversedData.forEach( (item) => {
-            
-            let copyOfPrevious = transformedData.slice(-1);
-
-            // console.log("the previous object",copyOfPrevious, "\n", "the current object", item);
-
-            if (copyOfPrevious[0]?.date !== item[2]) {
-                // Extract approval, disapproval, date from current item, map them to empty object. 
-                // Then add keys that generate the average approval/disapproval, by calculating sum of Array / length of Array
-                
-                let dataPoint = {
-                    date: undefined,
-                    approvalArray: [],
-                    disapprovalArray: [],
-                    averagedApproval: undefined,
-                    averagedDisapproval: undefined
-                };
+    const stream = fs.createReadStream("/Users/grant/Desktop/Trumpproval/src/convert/data/approval_topline.csv");
+function convert(results) {
     
-                dataPoint.date = item[2];
-                dataPoint.approvalArray.push(parseFloat(item[3]));
-                dataPoint.disapprovalArray.push(parseFloat(item[6]));
-                dataPoint.averagedApproval = getAverageRating(dataPoint.approvalArray);
-                dataPoint.averagedDisapproval = getAverageRating(dataPoint.disapprovalArray);
+    let reversedData = results.data.reverse();
+    reversedData.pop();
+    
+    // console.log("Parsing complete:", reversedData);
+    let transformedData = [];
 
-                transformedData.push(dataPoint);
-            } else {
-                // If the most recent object that was pushed shares the same date with current item, push approval and disapproval
-                // to approval and disapproval Arrays of most recent object
-                transformedData[transformedData.length - 1].approvalArray.push(parseFloat(item[3]));
-                transformedData[transformedData.length - 1].disapprovalArray.push(parseFloat(item[6]));
-                transformedData[transformedData.length - 1].averagedApproval = getAverageRating(transformedData[transformedData.length - 1].approvalArray);
-                transformedData[transformedData.length - 1].averagedDisapproval = getAverageRating(transformedData[transformedData.length - 1].disapprovalArray);
-            }
+    reversedData.forEach( (item) => {
+        
+        let copyOfPrevious = transformedData.slice(-1);
 
-        })
+        // console.log("the previous object",copyOfPrevious, "\n", "the current object", item);
 
-        transformedData.forEach( (item, index) => {
-            if(index !== 0) {
-                item.approvalChange = parseFloat((item.averagedApproval - transformedData[index - 1].averagedApproval).toFixed(2));
-                item.disapprovalChange = parseFloat((item.averagedDisapproval - transformedData[index - 1].averagedDisapproval).toFixed(2));
-            }
-        })
-        console.log(transformedData);
-        fs.writeFile("/Users/grant/Desktop/Trumpproval/src/convert/transformed_data/approval.json", JSON.stringify(transformedData), function (err) {
-            if (err) return console.log(err);
-            console.log('The data was written');
-          });
-        console.log("this should not come before");
-        // return transformedData;
-    }
+        if (copyOfPrevious[0]?.date !== item[2]) {
+            // Extract approval, disapproval, date from current item, map them to empty object. 
+            // Then add keys that generate the average approval/disapproval, by calculating sum of Array / length of Array
+            
+            let dataPoint = {
+                date: undefined,
+                approvalArray: [],
+                disapprovalArray: [],
+                averagedApproval: undefined,
+                averagedDisapproval: undefined
+            };
+
+            dataPoint.date = item[2];
+            dataPoint.approvalArray.push(parseFloat(item[3]));
+            dataPoint.disapprovalArray.push(parseFloat(item[6]));
+            dataPoint.averagedApproval = getAverageRating(dataPoint.approvalArray);
+            dataPoint.averagedDisapproval = getAverageRating(dataPoint.disapprovalArray);
+
+            transformedData.push(dataPoint);
+        } else {
+            // If the most recent object that was pushed shares the same date with current item, push approval and disapproval
+            // to approval and disapproval Arrays of most recent object
+            transformedData[transformedData.length - 1].approvalArray.push(parseFloat(item[3]));
+            transformedData[transformedData.length - 1].disapprovalArray.push(parseFloat(item[6]));
+            transformedData[transformedData.length - 1].averagedApproval = getAverageRating(transformedData[transformedData.length - 1].approvalArray);
+            transformedData[transformedData.length - 1].averagedDisapproval = getAverageRating(transformedData[transformedData.length - 1].disapprovalArray);
+        }
+
+    })
+
+    transformedData.forEach( (item, index) => {
+        if(index !== 0) {
+            item.approvalChange = parseFloat((item.averagedApproval - transformedData[index - 1].averagedApproval).toFixed(2));
+            item.disapprovalChange = parseFloat((item.averagedDisapproval - transformedData[index - 1].averagedDisapproval).toFixed(2));
+        }
+    })
+    console.log(transformedData.slice(0,20));
+    fs.writeFile("/Users/grant/Desktop/Trumpproval/src/convert/transformed_data/approval.json", JSON.stringify(transformedData), function (err) {
+        if (err) return console.log(err);
+        console.log('The data was written');
+        });
+    console.log("this should not come before");
+    // return transformedData;
+}
 
     papa.parse(stream, {
     
--- src/convert/convertTweets.js
+++ src/convert/convertTweets.js
@@ -1,6 +1,44 @@
-let tweets = require("/Users/grant/Desktop/Trumpproval/src/convert/data/TrumpTweets_Jan2017-_END - tweets_01-08-2021.csv");
-console.log(tweets.slice(0,100));
+const fs = require("fs");
+const papa = require("papaparse");
 
-function convert(data) {
-    // '
+const stream = fs.createReadStream("/Users/grant/Desktop/Trumpproval/src/convert/data/TrumpTweets_Jan2017-_END - tweets_01-08-2021.csv");
+
+function convert(results) {
+    console.log(results.data);
+
+    let data = results.data;
+    data.shift();
+
+    let transformedData = [];
+
+    data.forEach( (item) => {
+
+        let copyOfPrevious = transformedData.slice(-1);
+
+        if (copyOfPrevious[0]?.date !== item[7].split(' ')[0]) {
+            
+            let dataPoint = {
+                date: undefined,
+                numberOfTweets: 1
+            }
+
+            dataPoint.date = item[7].split(' ')[0];
+            transformedData.push(dataPoint);
+
+        } else {
+            transformedData[transformedData.length - 1].numberOfTweets += 1;
+        }
+    })
+    console.log(transformedData.slice(0,20));
+    fs.writeFile("/Users/grant/Desktop/Trumpproval/src/convert/transformed_data/tweets.json", JSON.stringify(transformedData), function (err) {
+        if (err) return console.log(err);
+        console.log('The data was written');
+        });
 }
+
+
+papa.parse(stream, {
+    
+    complete: (results) => convert(results)
+
+})
